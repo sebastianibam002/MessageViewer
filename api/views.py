bc6e02c459e296a7d736a960a1934a6202b7145a
sebastianibam002 <sebastianibam@gmail.com>
2022-02-09 00:20:26 -0500

    replace the #

--- api/views.py
+++ api/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 from home.models import StateOfDevice 
 # Create your views here.
 def _hex_to_rgb(hex):
-  rgb = []
-  for i in (0, 2, 4):
-    decimal = int(hex[i:i+2], 16)
-    rgb.append(decimal)
-  
-  return tuple(rgb)
+    hex = hex.replace("#", "")
+    rgb = []
+    for i in (0, 2, 4):
+        decimal = int(hex[i:i+2], 16)
+        rgb.append(decimal)
+    return tuple(rgb)
 
 def index(request):
     states_of_device = StateOfDevice.objects.all().last()
