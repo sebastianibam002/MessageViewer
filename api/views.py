5514ab05b0dcf9bae3d2c582bec94633805c5217
sebastianibam002 <sebastianibam@gmail.com>
2022-02-09 00:17:04 -0500

    change from hex to rgb values of color

--- api/views.py
+++ api/views.py
@@ -4,12 +4,21 @@ from django.http import HttpResponse
 
 from home.models import StateOfDevice 
 # Create your views here.
+def _hex_to_rgb(hex):
+  rgb = []
+  for i in (0, 2, 4):
+    decimal = int(hex[i:i+2], 16)
+    rgb.append(decimal)
+  
+  return tuple(rgb)
+
 def index(request):
     states_of_device = StateOfDevice.objects.all().last()
     response = {'message': 'Bom dia', 'led_one': '#ffffff', 'led_two': '#ffffff',
      'led_three': '#ffffff', 'led_four': '#ffffff'}
     if states_of_device is not None:
-        response = {'message': states_of_device.message , 'led_one': states_of_device.led_one_hex, 'led_two': states_of_device.led_two_hex,
-     'led_three': states_of_device.led_three_hex, 'led_four': states_of_device.led_four_hex}
+        red, green, blue = _hex_to_rgb(states_of_device.led_one_hex)
+        response = {'message': states_of_device.message , 'led_one': red, 'led_two': green,
+     'led_three': blue, 'led_four': states_of_device.led_four_hex}
 
     return JsonResponse(response)
